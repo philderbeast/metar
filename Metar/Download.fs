bb7a9f34595472604647ff4e586cf753eb3568a8
philderbeast <phil.dejoux@blockscope.com>
2012-05-21 16:30:14 +1000

    Put the download into a class and modify the test script to call the MetarFor static method on this class.

---
+++ Metar/Download.fs
@@ -0,0 +1,21 @@
+﻿namespace MeridianArc.Metar
+
+open System
+open System.IO
+open System.Net
+
+module internal _Download =
+    let reqHTTP uri = WebRequest.Create(uri : Uri) :?> HttpWebRequest
+
+    let req (cccc : string) =
+        let path = "http://weather.noaa.gov/pub/data/observations/metar/stations/" + cccc.ToUpper() + ".TXT"
+        let req = reqHTTP(new Uri(path))
+        let resp = req.GetResponse()
+        use stream = resp.GetResponseStream()
+        use sr = new StreamReader(stream, Text.Encoding.UTF8)
+        sr.ReadToEnd()
+        
+open _Download
+
+type Download() =
+    static member MetarFor(cccc) = req cccc 
--- Metar/Download.fsx
+++ Metar/Download.fsx
@@ -1,32 +1,5 @@
-﻿open System
-open System.IO
-open System.Net
-open System.Text
-open Microsoft.FSharp.Control.WebExtensions
+﻿#load "Download.fs"
 
-let reqFTP(uri : Uri) : FtpWebRequest =
-    let req = WebRequest.Create(uri)
-    let asFtp = req :?> FtpWebRequest
-    asFtp.Credentials <- new NetworkCredential("anonymous", "anonymous@meridianarc.com")
-    asFtp.UsePassive <- true
-    asFtp
+open System
 
-let req = reqFTP(new Uri("ftp://tgftp.nws.noaa.gov/data/observations/metar/stations/NZCH.TXT"))
-
-let resp = req.GetResponse()
-let stream = resp.GetResponseStream()
-let sr = new StreamReader(stream, Text.Encoding.UTF8)
-let contents = sr.ReadToEnd()
-sr.Close()
-stream.Close()
-
-let reqHTTP uri = WebRequest.Create(uri : Uri) :?> HttpWebRequest
-
-let req = reqHTTP(new Uri("http://weather.noaa.gov/pub/data/observations/metar/stations/NZAA.TXT"))
-
-let resp = req.GetResponse()
-let stream = resp.GetResponseStream()
-let sr = new StreamReader(stream, Text.Encoding.UTF8)
-let contents = sr.ReadToEnd()
-sr.Close()
-stream.Close()
+MeridianArc.Metar.Download.MetarFor "NZAA"
